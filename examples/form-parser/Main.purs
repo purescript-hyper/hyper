c1388131c8ab96e41ebb0233ee9092c545696463
Oskar Wickström <oskar.wickstrom@gmail.com>
2016-12-19 22:54:59 +0100

    Split up form parsing handler

--- examples/form-parser/Main.purs
+++ examples/form-parser/Main.purs
@@ -3,19 +3,21 @@ module Main where
 import Prelude
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Class (class MonadEff, liftEff)
 import Control.Monad.Eff.Console (log, CONSOLE)
-import Control.Monad.Eff.Exception (message, EXCEPTION)
+import Control.Monad.Eff.Exception (Error, message, EXCEPTION)
 import Data.Either (Either(Right, Left))
+import Data.Foldable (fold)
 import Data.Maybe (Maybe(Nothing, Just))
+import Data.MediaType.Common (textHTML)
 import Data.String (length)
 import Data.Tuple (lookup, Tuple(Tuple))
-import Hyper.Core (Port(Port))
+import Hyper.Core (closeHeaders, Middleware, Port(Port))
 import Hyper.Form (Form(Form), parseForm)
 import Hyper.HTML.DSL (p, text, element, html)
 import Hyper.Method (Method(POST, GET))
 import Hyper.Node.Server (readBodyAsString, defaultOptions, runServer)
-import Hyper.Response (respond, headers)
+import Hyper.Response (contentType, headers)
 import Node.HTTP (HTTP)
 
 main :: forall e. Eff (http :: HTTP, console :: CONSOLE, err :: EXCEPTION, avar :: AVAR | e) Unit
@@ -38,26 +40,30 @@ main =
                 Just s -> p [(Tuple "style" "color: red;")] (text s)
                 Nothing -> pure unit
 
+    handlePost body conn =
+      case body of
+        Left err -> do
+          liftEff (log (message err))
+          html (text "Bad request, invalid form.") conn
+        Right (Form values) ->
+          case lookup "firstName" values of
+            Just name | length name > 0 ->
+              html (p [] (text (fold ["Hi ", name, "!"]))) conn
+            _ -> renderNameForm (Just "Name is missing.") conn
+
     -- Our handler for GET and POST requests.
-    printForm conn =
+    handler conn =
       case conn.request.method of
         GET -> renderNameForm Nothing conn
-        POST ->
-          case conn.request.body of
-            Left err -> do
-              liftEff (log (message err))
-              respond "Bad request, invalid form.\n" conn
-            Right (Form values) ->
-              case lookup "firstName" values of
-                Just name | length name > 0 -> respond ("<p>Hi " <> name <> "!</p>") conn
-                _ -> renderNameForm (Just "Name is missing.") conn
-
-    -- Our handler always renders HTML.
-    handler = headers [(Tuple "Content-Type" "text/html")] >=> printForm
+        POST -> handlePost conn.request.body conn
 
     -- A chain of middleware for parsing the form, and then our response
     -- handler.
-    app = readBodyAsString >=> parseForm >=> handler
+    app = readBodyAsString
+          >=> parseForm
+          >=> contentType textHTML
+          >=> closeHeaders
+          >=> handler
 
     -- Some nice console printing when the server starts, and if a request
     -- fails (in this case when the request body is unreadable for some reason).
