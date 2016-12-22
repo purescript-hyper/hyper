a28b353f9e00dbc325c68f5fd6ab7eeedf977ddb
Oskar Wickström <oskar.wickstrom@gmail.com>
2016-12-22 21:10:57 +0100

    Make authentication middleware polymorphic

--- examples/authentication/Main.purs
+++ examples/authentication/Main.purs
@@ -3,6 +3,7 @@ module Main where
 import Prelude
 import Data.StrMap as StrMap
 import Node.Buffer as Buffer
+import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (class MonadEff, liftEff)
 import Control.Monad.Eff.Console (log, CONSOLE)
@@ -20,6 +21,46 @@ import Node.HTTP (HTTP)
 
 data User = User String
 
+class Authenticator a m t | a -> m where
+  addAuthentication
+    :: forall req res c.
+       a
+    -> Middleware
+        m
+        (Conn
+         { headers :: StrMap.StrMap String | req }
+         res
+         { authentication :: Unit | c })
+        (Conn
+         { headers :: StrMap.StrMap String | req }
+         res
+         { authentication :: Maybe t | c })
+
+  requireAuthentication
+    :: forall req res c rw.
+      (Monad m, ResponseWriter rw m) =>
+      a
+    -> Middleware
+       m
+       (Conn
+        { headers :: StrMap.StrMap String | req }
+        { writer :: rw StatusLineOpen | res }
+        { authentication :: t | c })
+       (Conn
+        { headers :: StrMap.StrMap String | req }
+        { writer :: rw ResponseEnded | res }
+        { authentication :: t | c })
+    -> Middleware
+      m
+      (Conn
+        { headers :: StrMap.StrMap String | req }
+        { writer :: rw StatusLineOpen | res }
+        { authentication :: Maybe t | c })
+      (Conn
+        { headers :: StrMap.StrMap String | req }
+        { writer :: rw ResponseEnded | res }
+        { authentication :: Unit | c })
+
 setAuthentication :: forall a b req res c.
                      b
                   -> Conn req res { authentication :: a | c }
@@ -27,80 +68,110 @@ setAuthentication :: forall a b req res c.
 setAuthentication auth conn =
   conn { components = (conn.components { authentication = auth })}
 
+data BasicAuth m t = BasicAuth (Tuple String String -> m (Maybe t))
 
-addAuthentication :: forall m e req res c a.
-                     (MonadEff (console :: CONSOLE, buffer :: BUFFER | e) m) =>
-                     (Tuple String String -> m (Maybe a))
-                  -> Middleware
-                     m
-                     (Conn
-                      { headers :: StrMap.StrMap String | req }
-                      res
-                      { authentication :: Unit | c })
-                     (Conn
-                      { headers :: StrMap.StrMap String | req }
-                      res
-                      { authentication :: Maybe a | c })
-addAuthentication mapper conn = do
-  auth <- getAuth
-  pure (setAuthentication auth conn)
-  where
-    decodeBase64 encoded =
-      liftEff (Buffer.fromString encoded Base64 >>= Buffer.toString ASCII)
-    splitPair s =
-      case split (Pattern ":") s of
-        [username, password] -> Just (Tuple username password)
-        _ -> Nothing
-    getAuth =
-      case StrMap.lookup "authorization" conn.request.headers of
-        Nothing -> pure Nothing
-        Just header -> do
-          case split (Pattern " ") header of
-            ["Basic", encoded] -> do
-              decoded <- splitPair <$> decodeBase64 encoded
-              case decoded of
-                Just auth -> mapper auth
-                Nothing -> pure Nothing
-            parts -> do
-              liftEff (log (show parts))
-              pure Nothing
+instance authenticatorBasicAuth :: (MonadEff (buffer :: BUFFER | e) m) =>
+                                   Authenticator (BasicAuth m t) m t where
+  addAuthentication (BasicAuth mapper) conn = do
+    auth <- getAuth
+    pure (setAuthentication auth conn)
+    where
+      decodeBase64 encoded =
+        liftEff (Buffer.fromString encoded Base64 >>= Buffer.toString ASCII)
+      splitPair s =
+        case split (Pattern ":") s of
+          [username, password] -> Just (Tuple username password)
+          _ -> Nothing
+      getAuth =
+        case StrMap.lookup "authorization" conn.request.headers of
+          Nothing -> pure Nothing
+          Just header -> do
+            case split (Pattern " ") header of
+              ["Basic", encoded] -> do
+                decoded <- splitPair <$> decodeBase64 encoded
+                case decoded of
+                  Just auth -> mapper auth
+                  Nothing -> pure Nothing
+              parts -> pure Nothing
 
+  requireAuthentication (BasicAuth _) mw conn =
+    case conn.components.authentication of
+      Nothing ->
+        writeStatus (Tuple 401 "Unauthorized") (setAuthentication unit conn)
+        >>= writeHeader (Tuple "WWW-Authenticate" "Basic realm=\"Hyper Authentication Example\"")
+        >>= closeHeaders
+        >>= respond "Please authenticate."
+      Just auth ->
+        setAuthentication auth conn
+        # mw
+        # map (setAuthentication unit)
 
-requireAuthentication
-  :: forall m req res c rw a.
-     (Monad m, ResponseWriter rw m) =>
+withAuthentication
+  :: forall m e req res c a t.
+     Authenticator a m t =>
      Middleware
      m
      (Conn
       { headers :: StrMap.StrMap String | req }
-      { writer :: rw StatusLineOpen | res }
-      { authentication :: a | c })
+      res
+      { authentication :: Unit, authenticator :: a | c })
      (Conn
       { headers :: StrMap.StrMap String | req }
+      res
+      { authentication :: Maybe t, authenticator :: a | c })
+withAuthentication conn =
+  addAuthentication conn.components.authenticator conn
+
+authenticated
+  :: forall req res c rw a m t.
+     (Monad m, ResponseWriter rw m, Authenticator a m t) =>
+     Middleware
+      m
+      (Conn
+      { headers :: StrMap.StrMap String | req }
+      { writer :: rw StatusLineOpen | res }
+      { authentication :: t, authenticator :: a | c })
+      (Conn
+      { headers :: StrMap.StrMap String | req }
       { writer :: rw ResponseEnded | res }
-      { authentication :: a | c })
+      { authentication :: t, authenticator :: a | c })
   -> Middleware
+    m
+    (Conn
+      { headers :: StrMap.StrMap String | req }
+      { writer :: rw StatusLineOpen | res }
+      { authentication :: Maybe t, authenticator :: a | c })
+    (Conn
+      { headers :: StrMap.StrMap String | req }
+      { writer :: rw ResponseEnded | res }
+      { authentication :: Unit, authenticator :: a | c })
+authenticated mw conn =
+  requireAuthentication conn.components.authenticator mw conn
+
+userFromBasicAuth :: forall e. Tuple String String -> Aff e (Maybe User)
+userFromBasicAuth =
+  case _ of
+    Tuple "admin" "admin" -> pure (Just (User "Administrator"))
+    _ -> pure Nothing
+
+myProfilePage
+  :: forall m req res c rw.
+     (Monad m, ResponseWriter rw m) =>
+     Middleware
      m
      (Conn
-      { headers :: StrMap.StrMap String | req }
+      req
       { writer :: rw StatusLineOpen | res }
-      { authentication :: Maybe a | c })
+      { authentication :: User | c })
      (Conn
-      { headers :: StrMap.StrMap String | req }
+      req
       { writer :: rw ResponseEnded | res }
-      { authentication :: Unit | c })
-requireAuthentication mw conn =
-  case conn.components.authentication of
-    Nothing ->
-      writeStatus (Tuple 401 "Unauthorized") (setAuthentication unit conn)
-      >>= writeHeader (Tuple "WWW-Authenticate" "Basic realm=\"Hyper Authentication Example\"")
-      >>= closeHeaders
-      >>= respond "Please authenticate."
-    Just auth ->
-      setAuthentication auth conn
-      # mw
-      # map (setAuthentication unit)
-
+      { authentication :: User | c })
+myProfilePage conn@{ components: { authentication: (User name) } } =
+  writeStatus statusOK conn
+  >>= contentType textHTML
+  >>= closeHeaders
+  >>= html (p [] (text ("You are authenticated as " <> name <> ".")))
 
 main :: forall e. Eff (console :: CONSOLE, http ∷ HTTP, buffer :: BUFFER | e) Unit
 main =
@@ -108,24 +179,9 @@ main =
     onListening (Port port) = log ("Listening on http://localhost:" <> show port)
     onRequestError err = log ("Request failed: " <> show err)
 
-    htmlWithStatus status x =
-      writeStatus status
-      >=> contentType textHTML
-      >=> closeHeaders
-      >=> html x
-
-    myProfilePage conn@{ components: { authentication: (User name) } } =
-      htmlWithStatus
-      statusOK
-      (p [] (text ("You are authenticated as " <> name <> ".")))
-      conn
-
-    userFromBasicAuth (Tuple username password) =
-      if username == "admin" && password == "admin"
-      then pure (Just (User username))
-      else pure Nothing
-
-    app = addAuthentication userFromBasicAuth
-          >=> requireAuthentication myProfilePage
-
-  in runServer defaultOptions onListening onRequestError { authentication: unit } app
+    app = withAuthentication >=> (authenticated myProfilePage)
+    components =
+      { authentication: unit
+      , authenticator: BasicAuth userFromBasicAuth
+      }
+  in runServer defaultOptions onListening onRequestError components app
