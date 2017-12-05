abba73f24bc8c3511be04ea5f5fcb458e8b366fe
Jimmy Hu <jimmyhu.co@gmail.com>
2017-12-05 23:42:44 +0800

    replace AVar with Ref and fix session id bug

--- src/Hyper/Node/Session/InMemory.purs
+++ src/Hyper/Node/Session/InMemory.purs
@@ -2,43 +2,47 @@ module Hyper.Node.Session.InMemory where
 
 import Prelude
 
-import Control.Monad.Aff (Aff)
-import Control.Monad.Aff.AVar (AVAR, AVar, makeVar, putVar, readVar, takeVar)
-import Control.Monad.Aff.Class (class MonadAff, liftAff)
-import Control.Monad.Aff.Console (CONSOLE, log)
+import Control.Monad.Eff.Class (class MonadEff, liftEff)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Random (RANDOM)
+import Control.Monad.Eff.Ref (REF, Ref, modifyRef, newRef, readRef)
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Newtype (unwrap)
 import Hyper.Session (class SessionStore, SessionID(..))
 
-data InMemorySessionStore session = InMemorySessionStore (AVar (Map SessionID session))
+data InMemorySessionStore session = InMemorySessionStore (Ref (Map SessionID session))
+
+foreign import generatedSessionID ::forall eff. Eff (random :: RANDOM | eff) String
 
 instance sessionStoreInMemorySessionStore :: ( Monad m
-                                             , MonadAff (avar :: AVAR, console :: CONSOLE | e) m
+                                             , MonadEff (ref:: REF, console :: CONSOLE, random :: RANDOM | e) m
                                              )
                                           => SessionStore
                                             (InMemorySessionStore session)
                                             m
                                             session where
-  newSessionID _ =
-    pure (SessionID "new-id")
+  newSessionID _ = do
+    id <- liftEff generatedSessionID
+    pure (SessionID id)
 
   get (InMemorySessionStore var) id =
-    liftAff do
+    liftEff do
       log ("Looking up session: " <> show (unwrap id))
-      Map.lookup id <$> readVar var
+      Map.lookup id <$> readRef var
 
   put (InMemorySessionStore var) id session = do
-    liftAff do
+    liftEff do
       log ("Saving session: " <> unwrap id)
-      Map.insert id session <$> takeVar var >>= flip putVar var
+      modifyRef var $ Map.insert id session
 
   delete (InMemorySessionStore var) id = do
-    liftAff do
+    liftEff do
       log ("Deleting session: " <> unwrap id)
-      Map.delete id <$> takeVar var >>= flip putVar var
+      modifyRef var $ Map.delete id
 
 newInMemorySessionStore
   :: forall e session
-   . Aff ( avar ∷ AVAR | e ) (InMemorySessionStore session)
-newInMemorySessionStore = InMemorySessionStore <$> makeVar Map.empty
+   . Eff ( ref∷ REF | e ) (InMemorySessionStore session)
+newInMemorySessionStore = InMemorySessionStore <$> newRef Map.empty
